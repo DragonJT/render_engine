79fa603d959e3bb5ea528ed4c1f2705e11282021
DragonJT <jonathanthoms006@gmail.com>
2024-03-30 17:02:36 +0000

    hello triangle

---
+++ src/main.rs
@@ -0,0 +1,118 @@
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+use wgpu::*;
+use futures::executor::block_on;
+use std::borrow::Cow;
+
+fn main() {
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let size = window.inner_size();
+    //GL loads faster
+    let instance = Instance::new(InstanceDescriptor{ backends:Backends::GL, ..Default::default()});
+    let surface = instance.create_surface(&window).unwrap();
+    let adapter = block_on(instance.request_adapter(&RequestAdapterOptions {
+        power_preference: PowerPreference::default(), 
+        compatible_surface: Some(&surface), 
+        force_fallback_adapter: false 
+    })).unwrap();
+    let (device, queue) = block_on(adapter.request_device(
+        &DeviceDescriptor { label: None, required_features: Features::empty(), required_limits: Limits::default()}, 
+        None)).unwrap();
+    let mut config = surface.get_default_config(&adapter, size.width, size.height).unwrap();
+    surface.configure(&device, &config);
+
+    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+    });
+
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: None,
+        bind_group_layouts: &[],
+        push_constant_ranges: &[],
+    });
+
+    let swapchain_capabilities = surface.get_capabilities(&adapter);
+    let swapchain_format = swapchain_capabilities.formats[0];
+
+    let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: None,
+        layout: Some(&pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &shader,
+            entry_point: "vs_main",
+            buffers: &[],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &shader,
+            entry_point: "fs_main",
+            targets: &[Some(swapchain_format.into())],
+        }),
+        primitive: wgpu::PrimitiveState::default(),
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        multiview: None,
+    });
+
+    let window = &window;
+    event_loop.run(move |event, target| {
+        let _ = (&instance, &adapter, &shader, &pipeline_layout);
+        if let Event::WindowEvent {
+            window_id: _,
+            event,
+        } = event
+        {
+            match event {
+                WindowEvent::Resized(new_size) => {
+                    config.width = new_size.width.max(1);
+                    config.height = new_size.height.max(1);
+                    surface.configure(&device, &config);
+                    window.request_redraw();
+                }
+                WindowEvent::RedrawRequested => {
+                    let frame = surface
+                        .get_current_texture()
+                        .expect("Failed to acquire next swap chain texture");
+                    let view = frame
+                        .texture
+                        .create_view(&wgpu::TextureViewDescriptor::default());
+                    let mut encoder =
+                        device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                            label: None,
+                        });
+                    {
+                        let mut rpass =
+                            encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                                label: None,
+                                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                                    view: &view,
+                                    resolve_target: None,
+                                    ops: wgpu::Operations {
+                                        load: wgpu::LoadOp::Clear(wgpu::Color::BLUE),
+                                        store: wgpu::StoreOp::Store,
+                                    },
+                                })],
+                                depth_stencil_attachment: None,
+                                timestamp_writes: None,
+                                occlusion_query_set: None,
+                            });
+                        rpass.set_pipeline(&render_pipeline);
+                        rpass.draw(0..3, 0..1);
+                    }
+
+                    queue.submit(Some(encoder.finish()));
+                    frame.present();
+                }
+                WindowEvent::CloseRequested => target.exit(),
+                _ => {}
+            };
+        }
+    })
+    .unwrap();
+}
+
